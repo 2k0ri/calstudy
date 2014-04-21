c61b770063f5381ca56793bd8e5f863ded2e6f3d
Kei Kori <esc13245@gmail.com>
2014-04-21 22:13:15 +0900

    DB設計案修正、READクエリ記述

--- calstudy.sql
+++ calstudy.sql
@@ -3,10 +3,9 @@ CREATE TABLE calstudy_users (
 	name VARCHAR(127),
 	email VARCHAR(255) NOT NULL UNIQUE,
 	update_at TIMESTAMP NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-	created_at TIMESTAMP NOT NULL default '0000-00-00 00:00:00',
-	is_deleted TINYINT(1) UNSIGNED default 0,
-	deleted_at TIMESTAMP NOT NULL default '0000-00-00 00:00:00',
-	INDEX (email, is_deleted)
+	created_at TIMESTAMP NOT NULL default 0,
+	deleted_at DATETIME,
+	INDEX (email, deleted_at)
 ) ENGINE = InnoDB;
 CREATE TABLE calstudy_tasks (
 	task_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -15,18 +14,16 @@ CREATE TABLE calstudy_tasks (
 	end_date TIMESTAMP,
 	task_title VARCHAR(255),
 	task_detail TEXT,
-	created_at TIMESTAMP NOT NULL default '0000-00-00 00:00:00',
-	is_deleted TINYINT(1) UNSIGNED default 0,
-	deleted_at TIMESTAMP NOT NULL default '0000-00-00 00:00:00',
-	INDEX (start_date, is_deleted)
+	created_at TIMESTAMP NOT NULL default 0,
+	deleted_at DATETIME,
+	INDEX (start_date, deleted_at)
 ) ENGINE = InnoDB;
 CREATE TABLE calstudy_user_task (
 	user_id INT UNSIGNED NOT NULL,
 	task_id INT UNSIGNED NOT NULL,
 	update_at TIMESTAMP NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-	created_at TIMESTAMP NOT NULL default '0000-00-00 00:00:00',
-	is_deleted TINYINT(1) UNSIGNED default 0,
-	deleted_at TIMESTAMP NOT NULL default '0000-00-00 00:00:00',
+	created_at TIMESTAMP NOT NULL default 0,
+	deleted_at DATETIME,
 	INDEX (user_id, task_id),
 	FOREIGN KEY (user_id) REFERENCES calstudy_users (user_id),
 	FOREIGN KEY (task_id) REFERENCES calstudy_tasks (task_id)
