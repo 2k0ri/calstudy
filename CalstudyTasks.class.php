dc3a25373a7b84ee8add354a58cd5077b8f31d08
Kei Kori <esc13245@gmail.com>
2014-04-21 20:47:37 +0900

    DB設計見直し、SQL修正

--- CalstudyTasks.class.php
+++ CalstudyTasks.class.php
@@ -6,20 +6,21 @@
 class CalstudyTasks
 {
     // 予定登録クエリ
-    const CREATE_QUERY_REGISTER = 'INSERT INTO calstudy_tasks (start_date, task_title, task_detail, created_date) VALUES (?, ?, ?, NOW())';
+    const CREATE_REGISTER_QUERY = 'INSERT INTO calstudy_tasks (start_date, task_title, task_detail) VALUES (?, ?, ?)';
     // 予定マッピングクエリ
-    const CREATE_QUERY_MAP = 'INSERT INTO calstudy_user_task (user_id, task_id) VALUES (?, LAST_INSERT_ID())';
+    const CREATE_MAP_QUERY = 'INSERT INTO calstudy_user_task (user_id, task_id) VALUES (?, LAST_INSERT_ID())';
     // ユーザー登録クエリ
-    const CREATE_USER = 'INSERT INTO calstudy_users (email, name) VALUES (?, ?)';
+    const CREATE_USER_QUERY = 'INSERT INTO calstudy_users (email, name) VALUES (?, ?)';
     // 予定読み込みクエリ
     // @TODO: INNER JOIN
-    const READ_QUERY = 'SELECT * FROM calstudy_tasks WHERE (start_date BETWEEN ? AND ?) AND state != \'deleted\'';
-    // @TODO: クエリ書き込み
-    // modified_dateにNOW()
-    const UPDATE_QUERY = '';
+    const READ_QUERY = 'SELECT * FROM calstudy_tasks WHERE (start_date BETWEEN ? AND ?) AND is_deleted = 0';
+    // 予定更新クエリ
+    const UPDATE_TASK_QUERY = 'UPDATE calstudy_tasks SET start_date = ?, end_date = ?, task_title = ?, task_detail = ? WHERE task_id = ?';
+    const UPDATE_USER_QUERY = 'UPDATE calstudy_users SET name = ?, email = ? WHERE user_id = ?';
     // 予定削除クエリ
     // DELETE文ではなく、stateを更新する論理削除
-    const DELETE_QUERY = '';
+    const DELETE_TASK_QUERY = 'UPDATE calstudy_tasks SET is_deleted = 1';
+    const DELETE_USER_QUERY = 'UPDATE calstudy_users SET is_deleted = 1';
 
     private $mysqli;
     private $user_id;
@@ -43,7 +44,7 @@ class CalstudyTasks
 
         if (!empty($email)) {
             // ユーザーIDを取得
-            $this->user_id = $this->mysqli->query('SELECT user_id FROM calstudy_users WHERE email = \''.$email.'\' AND state != \'deleted\'')->fetch_row();
+            $this->user_id = $this->mysqli->query('SELECT user_id FROM calstudy_users WHERE email = \''.$email.'\' AND is_deleted = 0')->fetch_row();
             $this->user_id = $this->user_id[0];
         }
     }
@@ -62,13 +63,13 @@ class CalstudyTasks
      */
     function createUser($email, $name)
     {
-        $stmt = $this->mysqli->prepare(CREATE_USER);
-
+        $stmt = $this->mysqli->prepare(self::CREATE_USER_QUERY);
         $stmt->bind_param('ss', $email, $name);
 
         $stmt->execute();
+        $this->user_id = $stmt->insert_id; // user_idメンバ変数にIDを格納
         $stmt->close();
-        return $this->user_id = $stmt->insert_id; // user_idメンバ変数にIDを格納
+        return $this->user_id;
     }
 
     /**
@@ -100,13 +101,13 @@ class CalstudyTasks
      * @param  string $task_details [description]
      * @return [type]               [description]
      */
-    function create($year, $month, $date, $task_title, $task_detail)
+    function createTask($year, $month, $date, $task_title, $task_detail)
     {
         // トランザクションのためオートコミットを無効化
         $this->mysqli->autocommit(FALSE);
 
         // 予定作成SQLのプリペアドステートメント設定
-        $stmt = $this->mysqli->prepare(CREATE_QUERY_REGISTER);
+        $stmt = $this->mysqli->prepare(CalstudyTasks::CREATE_REGISTER_QUERY);
         // TIMESTAMP型に合わせて時刻を設定
         $start_date = $this->encodeTimestamp($year, $month, $date);
 
@@ -127,7 +128,7 @@ class CalstudyTasks
         }
 
         // マッピングテーブル登録SQLのプリペアドステートメント設定
-        $stmt = $this->mysqli->prepare(CREATE_QUERY_MAP);
+        $stmt = $this->mysqli->prepare(CalstudyTasks::CREATE_MAP_QUERY);
         // プリペアドステートメント登録
         $stmt->bind_param('i', $this->user_id);
 
@@ -182,6 +183,7 @@ class CalstudyTasks
 }
 
 $calstudy_tasks = new CalstudyTasks();
+// @debug
 $calstudy_tasks->createUser('esc13245@gmail.com', '2k0ri');
-// $calstudy_tasks->create(2014, 4, 16, 'DB実装', 'データベース実装');
+$calstudy_tasks->createTask(2014, 4, 16, 'DB実装', 'データベース実装');
 var_dump($calstudy_tasks);
--- calstudy.sql
+++ calstudy.sql
@@ -2,28 +2,32 @@ CREATE TABLE calstudy_users (
 	user_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(127),
 	email VARCHAR(255) NOT NULL UNIQUE,
-	created_date TIMESTAMP, -- 初期化時にNULL→現在時刻、以降はUPDATEしない
-	state ENUM('created', 'updated', 'deleted') NOT NULL DEFAULT 'created',
-	state_updated_date TIMESTAMP, -- state更新時にNULL→現在時刻をUPDATE
-	INDEX (email, state)
+	update_at TIMESTAMP NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+	created_at TIMESTAMP NOT NULL default '0000-00-00 00:00:00',
+	is_deleted TINYINT(1) UNSIGNED default 0,
+	deleted_at TIMESTAMP NOT NULL default '0000-00-00 00:00:00',
+	INDEX (email, is_deleted)
 ) ENGINE = InnoDB;
 CREATE TABLE calstudy_tasks (
 	task_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	update_at TIMESTAMP NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
 	start_date TIMESTAMP,
 	end_date TIMESTAMP,
 	task_title VARCHAR(255),
 	task_detail TEXT,
-	created_date TIMESTAMP, -- 初期化時にNULL→現在時刻、以降はUPDATEしない
-	state ENUM('created', 'updated', 'finished', 'deleted') NOT NULL DEFAULT 'created',
-	state_updated_date TIMESTAMP, -- state更新時にNULL→現在時刻をUPDATE
-	INDEX (start_date, state)
+	created_at TIMESTAMP NOT NULL default '0000-00-00 00:00:00',
+	is_deleted TINYINT(1) UNSIGNED default 0,
+	deleted_at TIMESTAMP NOT NULL default '0000-00-00 00:00:00',
+	INDEX (start_date, is_deleted)
 ) ENGINE = InnoDB;
 CREATE TABLE calstudy_user_task (
 	user_id INT UNSIGNED NOT NULL,
 	task_id INT UNSIGNED NOT NULL,
-	INDEX (user_id, task_id)
+	update_at TIMESTAMP NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+	created_at TIMESTAMP NOT NULL default '0000-00-00 00:00:00',
+	is_deleted TINYINT(1) UNSIGNED default 0,
+	deleted_at TIMESTAMP NOT NULL default '0000-00-00 00:00:00',
+	INDEX (user_id, task_id),
+	FOREIGN KEY (user_id) REFERENCES calstudy_users (user_id),
+	FOREIGN KEY (task_id) REFERENCES calstudy_tasks (task_id)
 ) ENGINE = InnoDB;
--- CREATE TABLE calstudy_task_details (
--- 	task_id INT UNSIGNED NOT NULL PRIMARY KEY,
--- 	detail TEXT
--- ) ENGINE = InnoDB;
